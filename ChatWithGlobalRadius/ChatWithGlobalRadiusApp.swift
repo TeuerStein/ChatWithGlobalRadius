331058064c18dfdc4be655c182b5e1c50cfed432
TeuerStein <xenkai123456789@gmail.com>
2020-11-04 07:18:43 +0100

    Initial commit

---
+++ ChatWithGlobalRadius/ChatWithGlobalRadiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithGlobalRadiusApp.swift
+//  ChatWithGlobalRadius
+//
+//  Created by ALEXANDR DRAGUNOV on 11/4/20.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithGlobalRadiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
